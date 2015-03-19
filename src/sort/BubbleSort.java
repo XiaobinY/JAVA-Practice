19af9dcfb7b4b8c5b11bfa6efbafe9d42797715c
xiaobin <crist555@gmail.com>
2015-03-18 19:58:51 -0700

    Add BubbleSort

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,16 @@
+package sort;
+
+public class BubbleSort {
+	public static void bubbleSort(int[] input){
+		System.out.println(input.length);
+	}
+	
+	public static void main(String[] argus){
+		int[] intA = {1,2,3,4,5};
+		System.out.println("intA's length is " + intA.length);
+		
+		for(int i : intA){
+			System.out.println("Value is " + i);
+		}
+	}
+}
